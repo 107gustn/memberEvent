9e42f0298f5eb9059b4f68e61cc8eb3b0307b98f
khs <khs@k.k>
2022-06-09 21:00:47 +0900

    메인 추가 작업

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,10 +6,20 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		int num;
 		
 		while(true) {
 			System.out.println("1.회원관리 이동");
 			System.out.println("2.이벤트 관리 이동");
+			System.out.print(">>> ");
+			num = sc.nextInt();
+			
+			switch(num) {
+			case 1:
+				break;
+			case 2:
+				break;
+			}
 			
 		}
 
