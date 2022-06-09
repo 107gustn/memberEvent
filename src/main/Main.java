6af7c8505f10450215802a5db136f3db9f954757
khs <khs@k.k>
2022-06-09 20:36:33 +0900

    회원관리 이벤트 프로젝트

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("1.회원관리 이동");
+			System.out.println("2.이벤트 관리 이동");
+			
+		}
+
+	}
+
+}
